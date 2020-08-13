88c6a13877300d332b8b23fb2ff04e8b4da2a4c4
Raj Chaudhuri <rajch@hotmail.com>
2020-08-13 18:25:54 +0530

    To remove a golint warning.

--- pkg/vboxdriver/imagedefaults.go
+++ pkg/vboxdriver/imagedefaults.go
@@ -4,7 +4,7 @@ package vboxdriver
 // vboxdriver
 func defaultimages() map[string]*VBoxVMImage {
 	return map[string]*VBoxVMImage{
-		"1.16": &VBoxVMImage{
+		"1.16": {
 			ImageK8sVersion: "1.16",
 			ImageChecksum:   "495033e926241e4fed842acf7a892cb2c65c5f6cf7803d8a01af259f7162e07c",
 			ImageSourceURL:  "https://github.com/rajch/kutti-images/releases/download/v0.1.13-beta/kutti-0.1.13-k8s-1.16.ova",
