805c38bab3c7f783a4c72681c17fd723bf3e5f6c
Raj Chaudhuri <rajch@hotmail.com>
2020-09-12 11:25:10 +0530

    Removed width from STATUS field in drive ls command. Closes #6

--- cmd/kutti/cmd/driverls.go
+++ cmd/kutti/cmd/driverls.go
@@ -22,9 +22,9 @@ func init() {
 }
 
 func driverls(cmd *cobra.Command, args []string) {
-	fmt.Printf("%-12.12s  %-40.40s  %-12.12s\n", "NAME", "DESCRIPTION", "STATUS")
+	fmt.Printf("%-12.12s  %-40.40s  %s\n", "NAME", "DESCRIPTION", "STATUS")
 	clustermanager.ForEachDriver(func(driver *clustermanager.Driver) bool {
-		fmt.Printf("%-12.12s  %-40.40s  %-12.12s\n", driver.Name(), driver.Description(), driver.Status())
+		fmt.Printf("%-12.12s  %-40.40s  %s\n", driver.Name(), driver.Description(), driver.Status())
 		return false
 	})
 }
